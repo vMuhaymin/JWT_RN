a7bae0ac3cf54aa13fe6da6632ac93a31f4c68f9
vMuhaymin <Q9fd.67@gmail.com>
2026-05-24 12:41:45 +0300

    Fixing routing

--- frontend/auth/src/main.jsx
+++ frontend/auth/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter , Routes, Route} from 'react-router'
+import { BrowserRouter , Routes, Route, Navigate} from 'react-router'
 import './index.css'
 import App from './App.jsx'
 import Login from '../components/Login'
@@ -13,6 +13,10 @@ createRoot(document.getElementById('root')).render(
       <Route path="/" element={<App/>} />
       <Route path="/register" element={<Register/>} />
       <Route path="/login" element={<Login/>} />
+
+    {/* This will help to guid the user by enforce login and delete the old search history */}
+      <Route path='*' element={<Navigate to="/login" replace />} />
+
     </Routes>
   </BrowserRouter>,
 
