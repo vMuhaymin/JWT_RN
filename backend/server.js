2ea61c9b4e48850cdf0e081dd56f07f3960a0476
vMuhaymin <Q9fd.67@gmail.com>
2026-05-29 22:06:56 +0300

    Fixing crs

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,33 @@
 const express = require("express");
-const app = express()
+const cors = require('cors')
 
-app.get('/', (req, res)=>{
+const app = express()
+app.use(cors())
+
+
+const tempStorage = []
+
+
+
+app.post('/register', (req, res)=>{
+    const {username, password} = req.body;
+    if( !username || !password){
+        return res.status(400).json({ok:false, error: "One of the attribute is missing"})
+    }
+
+    const user = {
+        username,
+        password
+    }
+    user.name =req.body.username; user.password =req.body.password;
+    tempStorage.append(user)  
+    return res.status(200).json({HOLA : "user registered!"})
+});
+
+app.post('/login', (req, res)=>{
     return res.status(200).json({HOLA : "Welcome buddy!"})
 });
+
 
 
 const PORT = 3000;
