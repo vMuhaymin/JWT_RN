4df0b578272e8ccd0d0de293b8ac1a03296ca794
vMuhaymin <Q9fd.67@gmail.com>
2026-05-30 14:43:18 +0300

    Adding backend logic

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const cors = require('cors')
-
 const app = express()
 app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
-const tempStorage = []
 
-
+const tempStorage = [{username:"Ali",password:2312}]
 
 app.post('/register', (req, res)=>{
     const {username, password} = req.body;
+
     if( !username || !password){
         return res.status(400).json({ok:false, error: "One of the attribute is missing"})
     }
     const user = {
-        username,
-        password
+        username:"",
+        password:0
     }
-    user.name =req.body.username; user.password =req.body.password;
+    user.username = username; user.password = password;
     tempStorage.append(user)  
     return res.status(200).json({HOLA : "user has been registered!"})
 });
 
 app.post('/login', (req, res)=>{
-    const userInfo = req.body;
-    user = tempStorage.forEach((e)=>{
-        e.username == userInfo.username && e.password == userInfo.password
+    const {username, password} = req.body;
+    console.log( `The username is: ${username} | Password is ${password}` );
+
+    user = tempStorage.filter((e)=>{
+         e.username == username && e.password == password
     });
 
     if (!user) return res.status(400).json({ok: false, error: "user not found"})
